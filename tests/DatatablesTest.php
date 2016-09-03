576f9c716e6a00149de0f5f7f1ee1bd1be5bdccc
Mike Classic <mike@classichalton.com>
2016-09-03 15:37:29 -0400

    More unit tests

--- tests/DatatablesTest.php
+++ tests/DatatablesTest.php
@@ -46,4 +46,98 @@ class DatatablesTest extends PHPUnit_Framework_TestCase
         $this->assertArrayHasKey('draw', $output);
         $this->assertEquals($output['draw'], 23);
     }
+
+    public function test_array_push()
+    {
+        $datatables = new Datatables([], ['draw' => 1]);
+        $data = [
+            [
+                'id' => 1,
+                'name' => 'One'
+            ]
+        ];
+
+        $datatables->setData($data);
+        $total = $datatables->push(['id' => 2, 'name' => 'Two']);
+        $this->assertEquals(2, $total);
+
+        $output = json_decode($datatables->output(), true);
+        $rows = $output['data'];
+        $second = $rows[1];
+        $this->assertEquals(2, $second['id']);
+        $this->assertEquals('Two', $second['name']);
+    }
+
+    public function test_array_unshift()
+    {
+        $datatables = new Datatables([], ['draw' => 1]);
+        $data = [
+            [
+                'id' => 1,
+                'name' => 'One'
+            ]
+        ];
+
+        $datatables->setData($data);
+        $total = $datatables->unshift(['id' => 2, 'name' => 'Two']);
+        $this->assertEquals(2, $total);
+
+        $output = json_decode($datatables->output(), true);
+        $rows = $output['data'];
+        $first = $rows[0];
+        $this->assertEquals(2, $first['id']);
+        $this->assertEquals('Two', $first['name']);
+    }
+
+    public function test_array_pop()
+    {
+        $datatables = new Datatables([], ['draw' => 1]);
+        $data = [
+            [
+                'id' => 1,
+                'name' => 'One'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Two'
+            ]
+        ];
+
+        $datatables->setData($data);
+        $second = $datatables->pop();
+        $this->assertEquals(2, $second['id']);
+        $this->assertEquals('Two', $second['name']);
+
+        $output = json_decode($datatables->output(), true);
+        $rows = $output['data'];
+        $second = $rows[0];
+        $this->assertEquals(1, $second['id']);
+        $this->assertEquals('One', $second['name']);
+    }
+
+    public function test_array_shift()
+    {
+        $datatables = new Datatables([], ['draw' => 1]);
+        $data = [
+            [
+                'id' => 1,
+                'name' => 'One'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Two'
+            ]
+        ];
+
+        $datatables->setData($data);
+        $first = $datatables->shift();
+        $this->assertEquals(1, $first['id']);
+        $this->assertEquals('One', $first['name']);
+
+        $output = json_decode($datatables->output(), true);
+        $rows = $output['data'];
+        $second = $rows[0];
+        $this->assertEquals(2, $second['id']);
+        $this->assertEquals('Two', $second['name']);
+    }
 }
